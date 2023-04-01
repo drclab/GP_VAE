5d13c28b597f61a49e98932b26a3d163779fe15e
CJ Duan <Data.Scientist@dulun.com>
2023-03-31 23:00:13 -0400

    prompt

--- drclab/dulunGPT/auth.py
+++ drclab/dulunGPT/auth.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
+import openai
+
 PROFILE_DIR = Path(os.path.abspath(os.path.dirname(__file__))) 
 API_DIR = PROFILE_DIR.parent.parent
 
@@ -11,4 +13,20 @@ load_dotenv(key_file)
 
 api_key = os.getenv('API_KEY')
 
-print(api_key)
+openai.api_key = api_key
+
+prompt = 'who is Dr. Junhua Zhang'
+
+response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+)
+
+message = response.choices[0].text.strip()
+
+print(message)
+
