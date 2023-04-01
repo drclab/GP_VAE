3be2718e935e92d882e6edcc5d457e8ee3ebf25d
CJ Duan <Data.Scientist@dulun.com>
2023-04-01 16:30:14 -0400

    sys.argv

--- drclab/dulunGPT/auth.py
+++ drclab/dulunGPT/auth.py
@@ -1,3 +1,4 @@
+import sys
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -8,25 +9,35 @@ PROFILE_DIR = Path(os.path.abspath(os.path.dirname(__file__)))
 API_DIR = PROFILE_DIR.parent.parent
 
 key_file = API_DIR / 'chatgpt.env'
-
 load_dotenv(key_file)
 
 api_key = os.getenv('API_KEY')
 
 openai.api_key = api_key
 
-prompt = 'who is Dr. Junhua Zhang'
+default_prompt = 'How to use ChatGPT?'
 
-response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-)
+def answer(prompt):
 
-message = response.choices[0].text.strip()
+        response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=prompt,
+                max_tokens=1024,
+                n=1,
+                stop=None,
+                temperature=0.5,
+        )
 
-print(message)
+        return response.choices[0].text#.strip()
+
+
+def main(args):
+    print(answer(prompt=args))
+
+if __name__ == "__main__":
+        if len(sys.argv) > 1:
+                main(sys.argv[1])
+        else:
+                main('I have a dream')
+         
 
